240c86b1182d4cf9c1bdb27c6644aa823ca23dee
Brit Butler <redline6561@gmail.com>
2015-10-21 14:30:59 -0400

    Add a bit more structure.

--- lib/myhub.rb
+++ lib/myhub.rb
@@ -10,6 +10,23 @@ module Myhub
     set :logging, true
 
     # Your code here ...
+    get "/" do
+      api = Github.new
+      # get stuff from github
+      erb :index, locals: { issues: stuff }
+    end
+
+    put "/issue/:id" do
+      api = Github.new
+      api.reopen_issue(params["id"].to_i)
+      "Cool cool cool"
+    end
+
+    delete "/issue/:id" do
+      api = Github.new
+      api.close_issue(params["id"].to_i)
+      "Cool cool cool"
+    end
 
     run! if app_file == $0
   end
