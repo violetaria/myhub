3b114e078759ef642cb187a79d1a2a9ef636cb2b
Brit Butler <redline6561@gmail.com>
2015-10-21 14:15:32 -0400

    Add initializer to pull auth token from ENV.

--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -4,5 +4,11 @@ module Myhub
     base_uri "https://api.github.com"
 
     # Your code here too!
+    def initialize
+      @headers = {
+        "Authorization"  => "token #{ENV["AUTH_TOKEN"]}",
+        "User-Agent"     => "HTTParty"
+      }
+    end
   end
 end
